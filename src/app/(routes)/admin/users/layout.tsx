f5def9876faf2044bbc5ffa49710644a71c7b779
ocsoares <casoares016@gmail.com>
2023-11-03 22:25:54 -0300

    feat: add UsersLayout

--- src/app/(routes)/admin/users/layout.tsx
+++ src/app/(routes)/admin/users/layout.tsx
@@ -1,6 +1,19 @@
 import { PropsWithChildren } from "react";
 import { CheckIfUserJwtExpired } from "../../(with-auth)/components/CheckIfUserJwtExpired";
+import { Metadata } from "next";
+import { AppBar } from "@/shared/components/AppBar";
+
+export const metadata: Metadata = {
+  title: "Users",
+  description: "Users",
+};
 
 export default function UsersLayout({ children }: PropsWithChildren) {
-  return <CheckIfUserJwtExpired>{children}</CheckIfUserJwtExpired>;
+  return (
+    <>
+      <CheckIfUserJwtExpired>
+        <AppBar>{children}</AppBar>
+      </CheckIfUserJwtExpired>
+    </>
+  );
 }
