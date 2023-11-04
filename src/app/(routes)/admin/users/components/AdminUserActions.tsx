90720172edd98125c55abb475018f765f46b53fc
ocsoares <casoares016@gmail.com>
2023-11-04 12:15:06 -0300

    feat: update AdminUserActions

--- src/app/(routes)/admin/users/components/AdminUserActions.tsx
+++ src/app/(routes)/admin/users/components/AdminUserActions.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { useAdminUsers } from "../hooks/useAdminUsers";
 import { UserInfo } from "./UserInfo";
+import { AdminUserModal } from "./AdminUserModal";
 
 export function AdminUserActions() {
+  const { isModalOpen, handleOpenModal, handleCloseModal } = useAdminUsers();
+
   return (
     <>
-      <UserInfo text="Vitor Pereira" />
+      <UserInfo onClick={handleOpenModal} text="Vitor Pereira" />
+      <AdminUserModal
+        isModalOpen={isModalOpen}
+        handleCloseModal={handleCloseModal}
+      />
     </>
   );
 }
