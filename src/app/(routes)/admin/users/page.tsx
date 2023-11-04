d2bd6d27611cfb71ac65e935bf5cf7802984c96f
ocsoares <casoares016@gmail.com>
2023-11-03 22:30:30 -0300

    feat: add UsersPage page

--- src/app/(routes)/admin/users/page.tsx
+++ src/app/(routes)/admin/users/page.tsx
@@ -1,3 +1,42 @@
+import { Box, Container, Paper, Typography } from "@mui/material";
+import { AdminUserActions } from "./components/AdminUserActions";
+import { AppUserAvatar } from "./components/AppUserAvatar";
+
 export default function UsersPage() {
-  return <h2>PÁGINA DOS USERS !!</h2>;
+  return (
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "80vh",
+        minWidth: "100vh",
+        wordBreak: "break-word",
+      }}
+    >
+      <Paper
+        elevation={4}
+        sx={{
+          width: "120%",
+          padding: 10,
+          marginBottom: 5,
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <AppUserAvatar />
+          <Typography sx={{ mt: 2, fontSize: 34 }}>Usuários</Typography>
+          <AdminUserActions />
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
